6e6249cd7fecb8c187faaee8fdc06fe37a1fddc0
Reapex <tr7296228@gmail.com>
2021-01-17 13:36:57 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const path = require('path');
+const Cryptr = require('cryptr');
+const cryptr = new Cryptr('jfzdavzdauzdsazcauzdat242UF1U4141UF');
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+//Engine view
+app.set('view engine', 'ejs')
+app.engine('html', require('ejs').renderFile)
+
+// Static files
+app.use('/public', express.static(path.join(__dirname, "./public")));
+
+// Default route
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + '/pages/index.html');
+})
+
+// Joinning room
+app.post('/join_room', (req, res) => {
+    const username = req.body.username
+    res.redirect(`/room/${username}`)
+    res.end()
+})
+
+// Room Route
+app.get('/room/:tagId', function (req, res) {
+    id = req.params.tagId;
+    res.render(__dirname + '/pages/room.html', { id });
+
+    //User connecting to room 
+    io.on('connection', function (socket) {
+        socket.join(`room#${id}`);
+        console.log(`User joined room#${id}`);
+
+        console.log(io.in(`room#${id}`).allSockets())
+    });
+
+});
+
+// Messages
+io.on('connection', function (socket) {
+
+    socket.on('chat message', function (msg) {
+
+        var room_name = socket.request.headers.referer;
+        idRoom = room_name.split('/')[4]
+
+        io.to(`room#${idRoom}`).emit('chat message', msg);
+    })
+})
+
+// launch server
+server.listen(3000, function () {
+    console.log("Server running on 3000")
+})
